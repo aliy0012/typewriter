3d019b99a39b0eca2506359a4264734688d62483
aliy0012 <alikamal415@yahoo.com>
2021-11-02 19:37:36 +0000

    animating text by setTimeout function

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "Hello there from Lighthouse labs!";
+
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+  process.stdout.write(char);
+  }, delay);
+  delay += 50;
+}
+setTimeout(() => {
+console.log('');} , (delay + 10));
